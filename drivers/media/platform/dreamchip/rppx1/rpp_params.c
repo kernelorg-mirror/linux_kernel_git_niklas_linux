af0c965e73592137e8c9ff780b10bd77be34001e
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2026-09-09 10:16:44 +0200

    Revert "media: rppx1: Add support for Gamma Correction"
    This reverts commit 2475cd083b9785996019c87ee20d228fba1adc09.

--- drivers/media/platform/dreamchip/rppx1/rpp_params.c
+++ drivers/media/platform/dreamchip/rppx1/rpp_params.c
@@ -19,7 +19,6 @@ rkisp1_ext_params_blocks_info[] = {
 	RKISP1_PARAMS_BLOCK_INFO(BLS, bls),
 	RKISP1_PARAMS_BLOCK_INFO(AWB_GAIN, awb_gain),
 	RKISP1_PARAMS_BLOCK_INFO(CTK, ctk),
-	RKISP1_PARAMS_BLOCK_INFO(GOC, goc),
 	RKISP1_PARAMS_BLOCK_INFO(LSC, lsc),
 	RKISP1_PARAMS_BLOCK_INFO(AWB_MEAS, awb_meas),
 	RKISP1_PARAMS_BLOCK_INFO(HST_MEAS, hst),
@@ -66,9 +65,6 @@ int rppx1_params(struct rppx1 *rpp, struct vb2_buffer *vb, size_t max_size,
 			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_CTK:
 			module = &rpp->post.ccor;
-			break;
-		case RKISP1_EXT_PARAMS_BLOCK_TYPE_GOC:
-			module = &rpp->hv.ga;
 			break;
 		case RKISP1_EXT_PARAMS_BLOCK_TYPE_LSC:
 			module = &rpp->pre1.lsc;
